37ee449e2881a44694c85e403492c2b077f956c1
Shahi-Rep <fshahi.research@gmail.com>
2025-05-15 21:58:51 +0430

    Add files via upload

---
+++ Required_libraries.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Installing Python packages...
+
+pip install customtkinter
+pip install Pillow
+pip install ollama
+pip install pygame
+pip install PyPDF2
+pip install python-docx
+pip install pandas
+pip install charset-normalizer
+
+echo.
+echo All packages installed. Press any key to exit.
+pause >nul
